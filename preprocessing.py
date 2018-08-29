7bfc20722f796350abfc29e04851a97794872564
TitanEric <chenyihuang001@gmail.com>
2018-08-30 01:13:01 +0800

    Add chunks of audio

--- preprocessing.py
+++ preprocessing.py
@@ -13,8 +13,8 @@ import numpy as np
 
 def preprocess():
     # distribute_emotion()
-    distributeTrainTest()
-    # source2Chunks()
+    # distributeTrainTest()
+    source2Chunks()
     # normalizedAudio()
 
 def distribute_emotion():
@@ -106,18 +106,19 @@ def source2Chunks():
     Ref:
         - https://unix.stackexchange.com/questions/280767/how-do-i-split-an-audio-file-into-multiple
     """
-    source_dir = "wav"
+    source_dir = "origin"
     target_dir = "chunks"
     chunk_length = 0.02 # 20 ms
 
-    if not isdir(target_dir):
-        os.mkdir(target_dir)
-    
-    for audio_name in listdir(source_dir):
-        audio_prefix = audio_name.split(".")[0]
-        command = "ffmpeg -i {} -f segment -segment_time {} -c copy {}".format(
-            join(source_dir, audio_name), chunk_length, join(target_dir, "{}_%03d.wav".format(audio_prefix)))
-        subprocess.run(command, shell=True)
+    for attr in ["test", "train"]:
+        if not isdir(join(target_dir, attr)):
+            os.mkdir(join(target_dir, attr))
+        for audio_name in listdir(join(source_dir, attr)):
+            audio_prefix = audio_name.split(".")[0]
+            command = "ffmpeg -i {} -f segment -segment_time {} -c copy {}".format(
+                join(source_dir, attr, audio_name), chunk_length, 
+                join(target_dir, attr, "{}_%03d.wav".format(audio_prefix)))
+            subprocess.run(command, shell=True)
     
 
 
